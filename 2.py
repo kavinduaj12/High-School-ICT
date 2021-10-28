828e52ebeee92c4e8fa9f61ab880769c52d8fe37
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-10-28 07:00:45 +0530

    Added 2nd outputs

---
+++ 2.py
@@ -0,0 +1,29 @@
+from turtle import*
+
+
+# Output 1
+
+color("gray")
+pensize(5)
+begin_fill()
+for i in range(8):
+   right(45)
+   for i in range(6):
+       forward(50)
+       right(60)
+color("purple")
+end_fill()
+
+
+# Output 2
+
+color("blue")
+pensize(5)
+begin_fill()
+for i in range(8):
+   right(45)
+   for i in range(6):
+       forward(50)
+       right(60)
+color("black")
+end_fill()
