c16f282227c30ae9e390177f4bee9023fab535b1
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-10-28 07:07:01 +0530

    Add 4th outputs

---
+++ 4.py
@@ -0,0 +1,32 @@
+from turtle import*
+
+# Output 1
+
+color("purple")
+begin_fill()
+
+for i in range(8):
+   right(45)
+   for i in range(5):
+       forward(90)
+       dot(20)
+       right(72)
+       
+color("pink")
+end_fill()
+
+
+# Output 2
+
+color("yellow")
+begin_fill()
+
+for i in range(8):
+   right(45)
+   for i in range(5):
+       forward(90)
+       dot(20)
+       right(72)
+       
+color("blacl")
+end_fill()
