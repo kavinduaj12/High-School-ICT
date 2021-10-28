b816d0c265dac05627b348f2d7834442f8d35bc1
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-10-28 06:57:30 +0530

    Added a question and the answer

---
+++ 1.py
@@ -0,0 +1,28 @@
+from turtle import *
+
+
+# Question
+
+color("gray")
+pensize(5)
+begin_fill()
+for i in range(12):
+   right(30)
+   for i in range(4):
+       forward(90)
+       right(90)
+color("purple")
+end_fill()
+
+# Answer
+
+color("blue")
+pensize(5)
+begin_fill()
+for i in range(12):
+   right(30)
+   for i in range(4):
+       forward(90)
+       right(90)
+color("black")
+end_fill()
