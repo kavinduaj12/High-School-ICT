3742fe518b57f0b3b11506fa8ce59018c8ca2c5f
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-10-28 07:03:32 +0530

    Add 3rd outputs

---
+++ 3.py
@@ -0,0 +1,29 @@
+from turtle import*
+
+
+# Output 1
+
+color("gray")
+dot(220,"pink")
+begin_fill()
+for i in range(12):
+   right(30)
+   for i in range(8):
+       forward(40)
+       right(45)
+color("purple")
+end_fill()
+
+
+# Output 2
+
+color("blue")
+dot(220,"pink")
+begin_fill()
+for i in range(12):
+   right(30)
+   for i in range(8):
+       forward(40)
+       right(45)
+color("black")
+end_fill()
